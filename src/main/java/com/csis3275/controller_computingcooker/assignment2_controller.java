396f75090a6f73655aaba4dadafd6e777613f10f
Dat Tu <dattuminh@gmail.com>
2021-10-12 21:38:54 -0700

    working on model views

--- src/main/java/com/csis3275/controller_computingcooker/assignment2_controller.java
+++ src/main/java/com/csis3275/controller_computingcooker/assignment2_controller.java
@@ -1,12 +1,25 @@
 package com.csis3275.controller_computingcooker;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.csis3275.model_computingcooker.model_dtu_90;
 
 @Controller	
 public class assignment2_controller {
+	
 	@RequestMapping("/")
 	public String firstPage() {
-		return "index";
+		
+		return "aboutdavid";
+	}
+	
+	@PostMapping("/davidinfo/")
+	public String calBikeFee(model_dtu_90 david, ModelMap model) {
+		//model.addAttribute("path", david.getPath());
+		return "aboutdavid";
 	}
 }
