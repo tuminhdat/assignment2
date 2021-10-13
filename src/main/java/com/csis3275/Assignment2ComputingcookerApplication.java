34bdf5f5cf99d8a9ed451dce5371e67f61024df7
Dat Tu <dattuminh@gmail.com>
2021-10-12 20:29:33 -0700

    init commit

---
+++ src/main/java/com/csis3275/Assignment2ComputingcookerApplication.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2ComputingcookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment2ComputingcookerApplication.class, args);
+	}
+
+}
